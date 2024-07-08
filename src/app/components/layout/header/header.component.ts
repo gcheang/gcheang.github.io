73d24a3891f8e72f2dba5fb46694a28510a7f1bb
Gigi <gigi_cheang@hotmail.com>
2024-07-07 23:03:00 -0400

    Update site style, add dark mode switch, add scrolltop

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -1,27 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { RouterModule, Router } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { SidebarModule } from 'primeng/sidebar';
 import { fadeInAndOut } from '../../../animations/fadeInAndOut';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule, ButtonModule, SidebarModule],
+  imports: [RouterModule, ButtonModule, SidebarModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
   animations: [fadeInAndOut],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   sidebarVisible: boolean = false;
-  output = '';
+  darkMode: boolean = false;
 
-  constructor(private router: Router) {
-    router.events.subscribe(() => (this.sidebarVisible = false));
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe(() => (this.sidebarVisible = false));
+
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+      this.toggleDarkMode(true);
+    } else if (savedTheme === 'light') {
+      this.toggleDarkMode(false);
+    } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      this.toggleDarkMode(true);
+    }
+
+    console.log(this.darkMode);
   }
 
   showSidebar = () => {
     this.sidebarVisible = true;
   };
+
+  toggleDarkMode(bool: boolean) {
+    this.darkMode = bool;
+
+    if (bool) {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+    }
+  }
 }
--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ScrollTopModule } from 'primeng/scrolltop';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, ScrollTopModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
